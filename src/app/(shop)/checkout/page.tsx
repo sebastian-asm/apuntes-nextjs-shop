322fc80fbb7b0e143e8660e6c3a77417b2e22497
Sebastian Sanchez <sasanchezmiles@teco.com.ar>
2024-06-30 18:23:23 -0300

    pagina del carrito de compra

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,3 +1,65 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+import { Title } from '@/components'
+import { initialData } from '@/seed/seed'
+
 export default function CheckoutPage() {
-  return <section>Checkout Page</section>
+  const productsInCart = [initialData.products[0], initialData.products[1], initialData.products[2]]
+  return (
+    <section className="flex flex-col justify-center items-center mb-10 px-10 sm:px-0">
+      <Title title="Verificar compra" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 items-start">
+        <div>
+          <div className="mb-6">
+            <p>Detalle del carrito</p>
+            <Link href="/cart" className="underline">
+              Editar
+            </Link>
+          </div>
+          {productsInCart.map((product) => (
+            <div key={product.slug} className="flex mb-4">
+              <Image
+                src={`/products/${product.images[0]}`}
+                alt={product.title}
+                width="100"
+                height="100"
+                className="rounded object-cover"
+              />
+              <div className="ml-2">
+                <p>{product.title}</p>
+                <p>${product.price} x 3</p>
+                <p className="font-bold">Subtotal: ${product.price * 3}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="bg-white rounded-xl shadow-xl p-7">
+          <h2 className="text-2xl mb-2">Dirección de entrega</h2>
+          <div className="mb-4">
+            <p>Sebastian</p>
+            <p>Av. siempre viva 123</p>
+            <p>Santiago</p>
+          </div>
+          <hr />
+          <h2 className="text-2xl mb-2 mt-4">Detalle de la compra</h2>
+          <div className="grid grid-cols-2">
+            <p>Cantidad de productos:</p>
+            <p className="text-right">3</p>
+            <p>Subtotal:</p>
+            <p className="text-right">$123</p>
+            <p>Impuestos (19%):</p>
+            <p className="text-right">$123</p>
+            <p className="mt-4 text-xl">Total:</p>
+            <p className="text-right mt-4 text-xl">$123</p>
+          </div>
+          <div className="mt-6">
+            <Link href="/orders/123" className="btn-primary w-full block text-center">
+              Finalizar compra
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
